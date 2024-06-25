f0dc981396f376e10e90b14c125fec3bb457e312
Mustafa Sabra <mustafa_sabra@Ubuntu.myguest.virtualbox.org>
2024-06-25 12:59:30 +0300

    Adding the project documentation

---
+++ Project-Documentaion/html/search/all_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['calc_2ecpp_0',['calc.cpp',['../lib_2calc_8cpp.html',1,'(Global Namespace)'],['../unitTest_2calc_8cpp.html',1,'(Global Namespace)']]],
+  ['calc_2eh_1',['calc.h',['../lib_2calc_8h.html',1,'(Global Namespace)'],['../unitTest_2calc_8h.html',1,'(Global Namespace)']]]
+];
