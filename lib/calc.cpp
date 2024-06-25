80636b0cb89f64941c11ebf76fef11cd85af4c28
Mustafa Sabra <mustafa_sabra@Ubuntu.myguest.virtualbox.org>
2024-06-25 12:56:41 +0300

    Adding the library folder

---
+++ lib/calc.cpp
@@ -0,0 +1,25 @@
+
+/**
+ * @file calc.cpp
+ * @author Mustafa Sabra
+ * @brief 
+ * @version 0.1
+ * @date 2024-06-25
+ * 
+ * @copyright Copyright Mustafa-Sabra(c) 2024
+ * 
+ */
+
+#include "calc.h"
+
+/**
+ * @brief This is a simple function that adds two numbers and returns thier summation.
+ * 
+ * @param a (int number)
+ * @param b (int number)
+ * @return int 
+ */
+int sum(int a, int b)
+{
+    return (a+b);
+}
---
+++ lib/calc.h
@@ -0,0 +1,19 @@
+/**
+ * @file calc.h
+ * @author Mustafa Sabra
+ * @brief 
+ * @version 0.1
+ * @date 2024-06-25
+ * 
+ * @copyright Copyright Mustafa-Sabra(c) 2024
+ * 
+ */
+
+/**
+ * @brief This is a simple function that adds two numbers and returns thier summation.
+ * 
+ * @param a (int number)
+ * @param b (int number)
+ * @return int 
+ */
+int sum(int a, int b);
