dfdfe217a0009fb2c2e5ea6da4c9be66727c5c19
Mustafa Sabra <mustafa_sabra@Ubuntu.myguest.virtualbox.org>
2024-06-25 12:58:49 +0300

    Adding the build of the project

---
+++ build/lib/CMakeFiles/calc.dir/calc.c.o.d
@@ -0,0 +1,4 @@
+lib/CMakeFiles/calc.dir/calc.c.o: \
+ /home/mustafa_sabra/Documents/Gtest\ Labs/Gtest\ lab1/lib/calc.c \
+ /usr/include/stdc-predef.h \
+ /home/mustafa_sabra/Documents/Gtest\ Labs/Gtest\ lab1/lib/calc.h
---
+++ build/lib/CMakeFiles/calc.dir/calc.cpp.o.d
@@ -0,0 +1,4 @@
+lib/CMakeFiles/calc.dir/calc.cpp.o: \
+ /home/mustafa_sabra/Documents/Gtest\ Labs/Gtest\ lab1/lib/calc.cpp \
+ /usr/include/stdc-predef.h \
+ /home/mustafa_sabra/Documents/Gtest\ Labs/Gtest\ lab1/lib/calc.h
