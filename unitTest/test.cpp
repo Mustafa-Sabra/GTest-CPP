a22295e985d2e6bb35c4520fa6834345ed8f0745
Mustafa Sabra <mustafa_sabra@Ubuntu.myguest.virtualbox.org>
2024-06-25 13:06:15 +0300

    Updating the test.cpp to be documented

--- unitTest/test.cpp
+++ unitTest/test.cpp
@@ -1,3 +1,14 @@
+/**
+ * @file test.cpp
+ * @author Mustafa Sabra
+ * @brief 
+ * @version 0.1
+ * @date 2024-06-25
+ * 
+ * @copyright Copyright Mustafa-Sabra(c) 2024
+ * 
+ */
+
 #include <gtest/gtest.h>
     #include "calc.h"
 
@@ -7,16 +18,28 @@ extern "C"{
 */
 
 
-
+/**
+ * @brief Construct a new TEST suit with TESTCASE1
+ * 
+ */
 TEST(TEST_SUIT1, TESTCASE1)
 {
     EXPECT_EQ(sum(2,3),5);
 }
 
+/**
+ * @brief Construct a TEST suit with TESTCASE2
+ * 
+ */
+
 TEST(TEST_SUIT1, TESTCASE2)
 {
     EXPECT_EQ(sum(12,2),14);
 }
+/**
+ * @brief Construct a TEST suit with TESTCASE3
+ * 
+ */
 TEST(TEST_SUIT1, TESTCASE3)
 {
     EXPECT_NE(sum(2,14),5);
