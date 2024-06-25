26f884533612b0d23980e514008d7384238ad0ed
Mustafa Sabra <mustafa_sabra@Ubuntu.myguest.virtualbox.org>
2024-06-25 12:56:15 +0300

    Adding the main.cpp file

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file main.cpp
+ * @author Mustafa Sabra
+ * @brief 
+ * @version 0.1
+ * @date 2024-06-25
+ * 
+ * @copyright Copyright Mustafa-Sabra(c) 2024
+ * 
+ */
+
+#include <iostream>
+#include "calc.h"
+
+/**
+ * @brief This is the entry point of the application
+ * 
+ * @return int 
+ */
+int main(void)
+{
+    std::cout << "sum = " << sum(1,2) << std::endl;
+
+    return 0;
+}
